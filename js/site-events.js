d8f252b83c9a0af8895233c10edefd6e2256a3c8
Lenard Arreola <lenard.arreola@gmail.com>
2012-09-19 15:48:15 +0800

    added js files

---
+++ js/site-events.js
@@ -0,0 +1,26 @@
+$(document).ready(function(){
+var main = main = $('#mynav ul');
+
+$('.scroll').click(function(event) {
+        
+    event.preventDefault();
+ 
+    var full_url = this.href,
+        parts = full_url.split('#'),
+        trgt = parts[1],
+        target_offset = $('#'+trgt).offset(),
+        target_top = target_offset.top - 95;
+
+    $('html, body').animate({scrollTop:target_top}, 500);
+    
+    /* Remove active class on any li when an anchor is clicked */
+    
+    main.children().removeClass();
+    
+    /* Add active class to clicked anchor's parent li */
+        
+    $(this).parent().addClass('active');
+
+});
+
+});
